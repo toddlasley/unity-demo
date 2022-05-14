8e9ff0f500a1428da754f61083e2760c55171c97
toddlasley <todd.r.lasley@gmail.com>
2022-05-13 20:13:33 -0700

    Character respawns after falling in lava

--- Assets/Scripts/Character/CharacterCollision.cs
+++ Assets/Scripts/Character/CharacterCollision.cs
@@ -7,22 +7,14 @@ public class CharacterCollision : MonoBehaviour
 {
     private readonly string LAVA_GAME_OBJECT_NAME = "Lava";
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private readonly Vector3 RESPAWN_POSITION = new Vector3(3.696f, 4.58f, 8.172f);
 
     private void OnCollisionEnter(UnityEngine.Collision collision)
     {
         if (collision.gameObject.name.Equals(LAVA_GAME_OBJECT_NAME)) {
-            Debug.Log("You dead");
+            Debug.Log("Player fell into lava. Respawning...");
+            gameObject.transform.position = RESPAWN_POSITION;
+            Physics.SyncTransforms();
         }
     }
 }
